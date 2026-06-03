51a276c63d69c7c7b21a7f9199e7d42b4a9882b2
annus88 <annusnaveed114@gmail.com>
2026-06-03 18:08:21 +0500

    Final deployment update

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ from routes.auth_routes import *
 
 if __name__ == "__main__":
     app.run(
-        debug=True,
-        host="127.0.0.1",
-        port=5000
+    host="0.0.0.0",
+    port=5000,
+    debug=True
     )
